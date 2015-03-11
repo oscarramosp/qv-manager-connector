4340efd1afbb5bbcc0ba06546be6678b012ab8a7
oscarramosp@gmail.com <oscarramosp@gmail.com@922b11bc-d01b-ff29-4e95-ef86d7986687>
2015-03-11 23:04:32 +0000

    1ra versión

---
+++ trunk/QVManagerConnectorService/QVManagerConnectorHost/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace QVManagerConnectorHost
+{
+    public partial class Form1 : Form
+    {
+        private WebServiceHost host;
+        //http://localhost:8085/QVManagerApiConnectorService/ListarCALRaw
+
+        public Form1()
+        {
+            InitializeComponent();
+            host = new WebServiceHost(typeof(QVManagerConnectorService.QVManagerApiConnectorService));
+
+            host.Open();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lblMessage.Text = "Servicio iniciado";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            host = new WebServiceHost(typeof(QVManagerConnectorService.QVManagerApiConnectorService));
+
+            host.Open();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lblMessage.Text = "Servicio iniciado";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            host.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            lblMessage.Text = "Servicio detenido";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            host.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+}
